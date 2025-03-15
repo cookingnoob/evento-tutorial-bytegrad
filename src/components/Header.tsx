8dda141030b8d7b79bfa439b9625a4d8c0c77c41
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-03-14 23:34:24 -0600

    add logo component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
     <header>
+      <Logo />
       <Link href={"/"}>Home</Link>
       <Link href={"/events/all"}>All events</Link>
     </header>
