a28ed3ea1b0153f6398a2fe468b2ea316112e9a8
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-03-16 23:31:56 -0600

    add styles for main page, footer and header, add container component to wrap app and leave margin on the sides

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,23 @@
 import Link from "next/link";
-import React from "react";
+
+const routes = [
+  { path: "/privacy-policy", name: "Pivacy Policy" },
+  { path: "/terms-conditions", name: "Terms & Conditions" },
+];
 
 export default function Footer() {
   return (
-    <footer>
-      <Link href={"/privacy-policy"}>Pivacy Policy</Link>
-      <Link href={"/terms-conditions"}>Terms & Conditions</Link>
+    <footer className="mt-auto flex items-center justify-between h-16 border-t border-white/10 px-3 sm:px-9 text-xs text-white/25">
+      <small className="text-xs">
+        &copy; 2050 Bytegrad all rights reserved
+      </small>
+      <ul className="flex gap-x-3 sm:gap-x-8">
+        {routes.map((r) => (
+          <li key={r.path}>
+            <Link href={r.path}>{r.name}</Link>
+          </li>
+        ))}
+      </ul>
     </footer>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,27 @@ import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
 
+const routes = [
+  { href: "/", name: "Home" },
+  { href: "/events/all", name: "All events" },
+];
+
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <Link href={"/"}>Home</Link>
-      <Link href={"/events/all"}>All events</Link>
+      <nav>
+        <ul className="flex gap-x-6 text-sm">
+          {routes.map((r) => (
+            <li
+              key={r.href}
+              className="text-white/50 hover:text-white transition"
+            >
+              <Link href={r.href}>{r.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
