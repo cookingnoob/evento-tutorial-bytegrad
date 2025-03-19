5a265a92f3521b25198dd1ab57040cde2555bcd0
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-03-18 19:50:31 -0600

    add framer and clsx dependencies, add motion-framer div tag and style for nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const routes = [
   { href: "/", name: "Home" },
@@ -8,17 +12,30 @@ const routes = [
 ];
 
 export default function Header() {
+  const activePathName = usePathname();
   return (
     <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <nav>
-        <ul className="flex gap-x-6 text-sm">
+      <nav className="h-full">
+        <ul className="flex gap-x-6 h-full text-sm">
           {routes.map((r) => (
             <li
               key={r.href}
-              className="text-white/50 hover:text-white transition"
+              className={clsx(
+                "hover:text-white flex items-center relative transition",
+                {
+                  "text-white": activePathName === r.href,
+                  "text-white/50": activePathName !== r.href,
+                }
+              )}
             >
               <Link href={r.href}>{r.name}</Link>
+              {activePathName === r.href && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="bg-accent h-1 w-full absolute bottom-0"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
