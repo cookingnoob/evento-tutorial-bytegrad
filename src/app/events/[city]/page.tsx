77d79bb31a9388dcad8c5e3c238a9ffb88776d34
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-04-14 16:47:50 -0600

    add dynamic title that gets the city from the params and capitalizes first character, add fetching for the db, types for the data and map through it to render section tags with the corresponding name

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,9 +1,29 @@
 import H1 from "@/components/H1";
+import { EventoEvent } from "@/lib/types";
 
-export default function EventsCityPage() {
+type EventCityPagePros = {
+  params: {
+    city: string;
+  };
+};
+
+export default async function EventsCityPage({ params }: EventCityPagePros) {
+  const city = params.city;
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/evento/api/events/?city=austin"
+  );
+
+  const events: EventoEvent[] = await response.json();
+
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
-      <H1>Events in Austin</H1>
+      {city === "all" && <H1>All Events</H1>}
+      {city !== "all" && (
+        <H1>Events in {city.charAt(0).toLocaleUpperCase() + city.slice(1)}</H1>
+      )}
+      {events.map((event) => (
+        <section key={event.id}>{event.name}</section>
+      ))}
     </main>
   );
 }
