3c59424f688edbfedeb7bccf680a91f145944e09
cookingnoob <{ID}+{username}@users.noreply.github.com>
2025-04-21 08:09:02 -0600

    add skeleton and skeleton card to style the loading screens

--- src/app/event/[slug]/loading.tsx
+++ src/app/event/[slug]/loading.tsx
@@ -1,5 +1,12 @@
+import Skeleton from "@/components/skeleton";
 import React from "react";
 
 export default function Loading() {
-  return <div>Loading city...</div>;
+  return (
+    <div className="flex flex-col items-center gap-y-4 pt-28">
+      <Skeleton className="h-4 w-[550px]" />
+      <Skeleton className="h-4 w-[400px]" />
+      <Skeleton className="h-4 w-[430px]" />
+    </div>
+  );
 }
--- src/app/events/[city]/loading.tsx
+++ src/app/events/[city]/loading.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import SkeletonCard from "@/components/skeleton-card";
 
 export default function Loading() {
-  return <div>Loading events...</div>;
+  return (
+    <div className="flex flex-wrap justify-center max-w-[1100px] mx-auto px-[20px] py-24 gap-20">
+      {Array.from({ length: 6 }).map((e, index) => (
+        <SkeletonCard key={index} />
+      ))}
+    </div>
+  );
 }
